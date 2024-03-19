a279a57d3f3d1ac085ebe66389602623bd64e22b
SarahBatagioti <sarah.montuanibt@gmail.com>
2024-03-18 21:48:47 -0300

    desafio 01

---
+++ desafio 01.py
@@ -0,0 +1,17 @@
+# Observe o trecho de código abaixo:
+# int INDICE = 13, SOMA = 0, K = 0;
+# enquanto K < INDICE faça
+# {
+# K = K + 1;
+# SOMA = SOMA + K;
+# }
+# imprimir(SOMA);
+
+i = 13
+s = 0
+k = 0
+
+while k < i:
+    k = k + 1
+    s = s + 1
+print(s)
